718874300f28f7961cb4c3768ccf511dc126875b
Kiersten Mikulsky <kmikulsky21@gmail.com>
2021-07-28 00:50:50 -0400

    fixed assets file structure, placed style refs in header and scripts in footer

--- front-page.php
+++ front-page.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="en"> 
 <head>
-    <title>Blog Site Template</title>
+    <title>FX Physical Therapy</title>
     <!-- Meta -->
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -9,11 +9,9 @@
     <meta name="description" content="Blog Site Template">
     <meta name="author" content="https://youtube.com/FollowAndrew">    
     <link rel="shortcut icon" href="images/logo.png"> 
-    
 	<?php
 	wp_head();
 	?>
-
 </head> 
 
 <body>
@@ -125,13 +123,11 @@
 	    </footer>
     
     </div>
-    
-    
-    <!-- Bootstrap Javascript -->          
-	<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
-	<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
-	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
-	<script src="js/main.js"></script>
+
+    <?php
+		wp_footer();
+	?>
+   
 
 </body>
 </html> 
--- functions.php
+++ functions.php
@@ -1,13 +1,27 @@
 <?php
 
 function fxphysical_register_styles(){
+    $version = wp_get_theme()->get('Version');
     //the line below commnted out isnt working so we need a workaround for the array order. this is a common issue with local host 
-    //wp_enqueue_style('fxphysical-style' , get_template_directory_uri() . '/FX_Physical_Therapy/style.css', array() ,'1.0','all');
-      wp_enqueue_style('fxphysical-style' , get_stylesheet_uri() ); 
-      wp_enqueue_style('fxphysical-bootstrap' , "https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" , array(), '1.0', 'all' );
-      wp_enqueue_style('fxphysical-fontawesome' , "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css" , array(), '1.0', 'all');
+    //wp_enqueue_style('fxphysical-style' , get_template_directory_uri() . '/FX_Physical_Therapy/style.css', array() ,$version,'all');
+      wp_enqueue_style('fxphysical-bootstrap' , "https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" , array(), '4.4.1', 'all' );
+      wp_enqueue_style('fxphysical-style' , get_stylesheet_uri()); 
+      wp_enqueue_style('fxphysical-fontawesome' , "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css" , array(), '5.13.0', 'all');
 }
 
 add_action('wp_enqueue_scripts', 'fxphysical_register_styles');
+//add_theme_support( 'admin-bar', array( 'callback' => '__return_false' ) );
+
+function fxphysical_register_scripts(){
+   
+    wp_enqueue_script('fxphysical-jquery','https://code.jquery.com/jquery-3.4.1.slim.min.js', array(), '3.4.1', true );
+    wp_enqueue_script('fxphysical-popper','https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js', array(), '1.16.0', true );
+    wp_enqueue_script('fxphysical-bootstrap','https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js', array(), '4.4.1', true );
+    wp_enqueue_script('fxphysical-main',get_template_directory_uri()."/assets/js/main.js", array(), '1.0', true );
+
+}
+
+add_action('wp_enqueue_scripts', 'fxphysical_register_scripts');
+
 
 ?>
