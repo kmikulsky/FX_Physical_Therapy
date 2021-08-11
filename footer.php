22424b5e1bd9ac08fb959e43b31f87026cd09ab7
Kiersten Mikulsky <kmikulsky21@gmail.com>
2021-08-11 00:36:57 -0400

    added 404 and search content. can now search for pages on 404

--- footer.php
+++ footer.php
@@ -1,33 +1,14 @@
 <footer class="footer text-center py-2 theme-bg-dark">
-   <br></br>
+   
+   <?php
+   dynamic_sidebar('footer-1');
+   ?>
+
+</footer>
+
 <?php
-
-if ( function_exists('the_custom_logo') ){
-   $custom_logo_id = get_theme_mod('custom_logo');
-   $logo = wp_get_attachment_image_src($custom_logo_id);
-}
-
+	wp_footer();
 ?>
-
-<img class="mb-3 mx-auto logo" src="<?php echo $logo[0] ?>" alt="logo" >  
-<br></br>
-          <!-- <p class="copyright"><a href=">Further Information</a></p> -->
-            <ul id="samplefooter">
-                <li>About</li>
-                <li>Services</li>
-                <li>Conditions We Treat</li>
-                <li>Locations</li>
-                <li>FX Brands</li>
-                <li>Blog</li>
-                <li>Online Payment</li>
-            </ul>  
-
-        
-       </div>
-
-    <?php
-		wp_footer();
-	?>
     
 </body>
 </html> 
--- functions.php
+++ functions.php
@@ -44,5 +44,34 @@ function fxphysical_register_scripts(){
 
 add_action('wp_enqueue_scripts', 'fxphysical_register_scripts');
 
+function fxphysical_widget_areas(){
+
+    register_sidebar(
+        array(
+            'before_title' => '<h2>' ,
+            'after_title' => '</h2>',
+            'before_widget' => '<ul class="social-list list-inline py-3 mx-auto">',
+            'after_widget' => '</ul>',
+            'name' => 'Sidebar Area',
+            'id' => 'sidebar-1',
+            'description' => 'Sidebar Widget Area'
+        )
+    );
+
+    register_sidebar(
+        array(
+            'before_title' => '<h2>' ,
+            'after_title' => '</h2>',
+            'before_widget' => '<ul class="social-list list-inline py-3 mx-auto">',
+            'after_widget' => '</ul>',
+            'name' => 'Footer Area',
+            'id' => 'footer-1',
+            'description' => 'Footer Widget Area'
+        )
+    );
+}
+
+add_action( 'widgets_init', 'fxphysical_widget_areas');
+
 
 ?>
